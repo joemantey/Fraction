754918126bc4cec3473b2d9bbe474e90e37a04d4
joemantey <joemantey@gmail.com>
2015-09-30 11:17:40 -0400

    formatted home page

---
+++ Fraction/JSHomeTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  JSHomeTableViewController.h
+//  Fraction
+//
+//  Created by Joseph Smalls-Mantey on 9/30/15.
+//  Copyright (c) 2015 Yosimite Labs | Joseph Smalls-Mantey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JSHomeTableViewController : UITableViewController
+
+@end
--- Fraction/JSVenmoAPIClient.m
+++ Fraction/JSVenmoAPIClient.m
@@ -25,28 +25,10 @@ static JSVenmoAPIClient * venmoAPIClient;
 
 #pragma mark - Get Authorization
 
-+(void)getVenmoAuthorizationCodeForUser:()
 
 #pragma mark - Send Money
 
-+(void)sendMoneyWithID:(NSString*)IDString andNote:(NSString*)note andAmount:(NSString*)amount andAudience:(NSString *)audience Completion:(void (^)(NSDictionary *))completionBlock{
-    
-    NSString *sqootURL = VENMO_SANDBOX_POST_PAYMENT_URL;
-    NSDictionary *parameterDictionary = @{@"api_key":VENMO_API_APP_ID};
-    
-    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
-    
-    [manager GET:sqootURL parameters:parameterDictionary success:^(NSURLSessionDataTask *task, id responseObject)
-     {
-         NSDictionary *dealQueryResponseDictionary= ((NSDictionary *)responseObject)[@"deal"];
-         completionBlock(dealQueryResponseDictionary);
-     }
-     
-         failure:^(NSURLSessionDataTask *task, NSError *error){
-             NSLog(@"Failed to search: %@", error.localizedDescription);
-         }];
-    
-}
+
 
 
 @end
