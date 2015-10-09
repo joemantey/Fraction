f1cc672922f891cca2cbe42245f18802833c9194
joemantey <joemantey@gmail.com>
2015-10-08 20:53:22 -0400

    start laying out search for contact view

---
+++ Fraction/JSContactSearchViewController.h
@@ -0,0 +1,13 @@
+//
+//  JSContactSearchViewController.h
+//  Fraction
+//
+//  Created by Joseph Smalls-Mantey on 10/8/15.
+//  Copyright (c) 2015 Yosimite Labs | Joseph Smalls-Mantey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JSContactSearchViewController : UIViewController
+
+@end
